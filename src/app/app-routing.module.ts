12fd7d0e483197cc271c3ed85a2323674341b5c2
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-01-15 13:12:32 +0100

    feat:add product home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactHomeComponent } from './contacts/contact-home/contact-home.component';
+import { ProductHomeComponent } from './products/product-home/product-home.component';
 
-const routes: Routes = [{path: 'contacts', component: ContactHomeComponent }];
+const routes: Routes = [{path: 'contacts', component: ContactHomeComponent }, {path: 'products', component: ProductHomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,31 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactHomeComponent } from './contacts/contact-home/contact-home.component';
+
 import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactHomeComponent } from './contacts/contact-home/contact-home.component';
+import { ProductHomeComponent } from './products/product-home/product-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactHomeComponent
+    ContactHomeComponent,
+    ProductHomeComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatTableModule,
-    HttpClientModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
