70b17c771315bd98c53b43112441b729bd3f8c03
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-01-16 12:11:32 +0100

    feat: implements navigation to product details

--- src/app/products/product-home/product-home.component.ts
+++ src/app/products/product-home/product-home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { IProduct } from 'src/app/models/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-home',
@@ -10,11 +11,15 @@ import { IProduct } from 'src/app/models/product';
 export class ProductHomeComponent {
   products!: IProduct[];
 
-  constructor(private productService: ProductsService){}
+  constructor(private productService: ProductsService, private router:Router){}
 
     ngOnInit(): void {
       this.productService
       .getProducts()
       .subscribe((data: IProduct[]) => (this.products = data));
     }
+
+  openDetailForm(row:IProduct){
+    this.router.navigate(['/products', row.id]);
+  }
 }
