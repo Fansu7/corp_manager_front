e0b5edac3079384e73670f00aee5ace2bba24814
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-01-27 12:13:41 +0100

    feat: add products charts to the chart component

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../contacts/contacts.service';
 import { IContact } from '../models/contact';
+import { IProduct } from '../models/product';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-charts',
@@ -9,11 +11,19 @@ import { IContact } from '../models/contact';
 })
 export class ChartsComponent implements OnInit {
   initialLetter: any[] = [];
+  initialLetterP: any[] = [];
   contactsByFullName: any;
+  productsByFullName: any;
   emailExtensions: any;
   phonePrefixData!: [];
+  productsAvailability: any;
+  productsStock: any;
+  productsPrices: any;
 
-  constructor(private contactsService: ContactsService) {}
+  constructor(
+    private contactsService: ContactsService,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit(): void {
     this.contactsService.getContacts().subscribe((contacts: IContact[]) => {
@@ -21,6 +31,13 @@ export class ChartsComponent implements OnInit {
       this.contactsByFullName = this.calculateContactsByFullName(contacts);
       this.emailExtensions = this.calculateEmailExtensionsData(contacts);
       this.phonePrefixData = this.generatePhonePrefixData(contacts);
+    });
+    this.productsService.getProducts().subscribe((products: IProduct[]) => {
+      this.initialLetterP = this.calculateInitialLettersDataP(products);
+      this.productsByFullName = this.calculateProductsByFullName(products);
+      this.productsAvailability = this.calculateProductsAvailability(products);
+      this.productsStock = this.calculateProductsStock(products);
+      this.productsPrices = this.calculateProductsPrice(products);
     });
   }
 
@@ -45,6 +62,35 @@ export class ChartsComponent implements OnInit {
     });
 
     return tempContactsByFullName.map((entry) => {
+      return {
+        ...entry,
+        series: entry.series.sort(
+          (a, b) => Number(a.name.split('-')[0]) - Number(b.name.split('-')[0])
+        ),
+      };
+    });
+  }
+
+  calculateProductsByFullName(products: IProduct[]): any {
+    const tempProductsByFullName: { name: string; series: any[] }[] = [
+      { name: 'Products', series: [] },
+    ];
+
+    products.forEach((product) => {
+      const size = product.name.length;
+      const range = `${size - (size % 5)}-${size - (size % 5) + 4} ch.`;
+
+      const existingRange = tempProductsByFullName[0].series.find(
+        (item) => item.name === range
+      );
+      if (existingRange) {
+        existingRange.value++;
+      } else {
+        tempProductsByFullName[0].series.push({ name: range, value: 1 });
+      }
+    });
+
+    return tempProductsByFullName.map((entry) => {
       return {
         ...entry,
         series: entry.series.sort(
@@ -92,6 +138,58 @@ export class ChartsComponent implements OnInit {
       }
       return result.sort((a: any, b: any) => (a.name > b.name ? 1 : -1));
     }, []);
+  }
+
+  calculateInitialLettersDataP(products: IProduct[]): IProduct[] {
+    return products.reduce((result: any, product: IProduct) => {
+      const initial = product.name.charAt(0).toUpperCase();
+      if (result.find((item: IProduct) => item.name === initial)) {
+        result.find((item: IProduct) => item.name === initial).value++;
+      } else {
+        result.push({ name: initial, value: 1 });
+      }
+      return result.sort((a: any, b: any) => (a.name > b.name ? 1 : -1));
+    }, []);
+  }
+
+  calculateProductsAvailability(products: IProduct[]) {
+    const availableMap: Map<string, number> = new Map<string, number>();
+    products.forEach((product) => {
+      const productAvailable = product.active ? 'Available' : 'Not available';
+      if (availableMap.has(productAvailable)) {
+        availableMap.set(
+          productAvailable,
+          (availableMap.get(productAvailable) ?? 0) + 1
+        );
+      } else {
+        availableMap.set(productAvailable, 1);
+      }
+    });
+
+    const availableProducts: any = [];
+    availableMap.forEach((value: number, key: string) => {
+      availableProducts.push({ name: key, value: value });
+    });
+    return availableProducts;
+  }
+
+  calculateProductsPrice(products: IProduct[]): IProduct[] {
+    const pricesArray: any = [];
+    products.forEach((product) => {
+      pricesArray.push({
+        name: product.name,
+        value: Math.round(product.price),
+      });
+    });
+    return pricesArray.sort((a: any, b: any) => (a.value > b.value ? 1 : -1));
+  }
+
+  calculateProductsStock(products: IProduct[]) {
+    const stockArray: any = [];
+    products.forEach((product) => {
+      stockArray.push({ name: product.name, value: product.stock });
+    });
+    return stockArray.sort((a: any, b: any) => (a.value < b.value ? 1 : -1));
   }
 
   generatePhonePrefixData(contacts: IContact[]) {
